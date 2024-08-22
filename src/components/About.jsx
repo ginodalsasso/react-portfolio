e37cdfc0590ac3986655dbad1592c180d3957fa0
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-08-22 22:18:31 +0200

    section wrapper done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-// import { SectionWrapper } from "../hoc";
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { Tilt } from "react-tilt";
 // import { div } from "three/examples/jsm/nodes/Nodes.js";
@@ -58,26 +58,4 @@ const About = () => {
     );
 };
 
-export default About;
-
-// import React from "react";
-// import Tilt from "react-tilt";
-// import { motion } from "framer-motion";
-
-// import { styles } from "../styles";
-// import { services } from "../constants";
-// // import { SectionWrapper } from "../hoc";
-// import { fadeIn, textVariant } from "../utils/motion";
-
-// const About = () => {
-//   return (
-//     <>
-//     <motion.div>
-//       <p className={styles.sectionSubText}>Introduction</p>
-//       <h2 className={styles.sectionHeadText}>Overview</h2>
-//     </motion.div>
-//     </>
-//   )
-// }
-
-// export default About
+export default SectionWrapper(About, "about");
