ec6688b3f9303c06f6fc4b3da709029b722bcf15
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-08-23 15:58:17 +0200

    Experience section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,32 +1,45 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+// import { Bloom, SSAO, DepthOfField } from "@react-three/postprocessing";
+
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-    const computer = useGLTF("./desktop_pc/scene.gltf");
+    const computer = useGLTF("./commodore_64/scene.gltf");
 
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor="black" />
-            <spotLight
-                position={[-10, 20, 10]} // Rapproche la lumière pour un effet plus visible
-                angle={0.3} // Augmente l'angle pour couvrir une plus grande surface
-                penumbra={0.5} // Modifie le flou des ombres
-                intensity={2} // Augmente l'intensité pour plus de luminosité
-                castShadow // Active les ombres
+            {/* Ajoute les lumières à la scène */}
+            <hemisphereLight intensity={0.4} groundColor="black" />
+            <directionalLight // Ajoute une lumière directionnelle
+                color="#f5d69d"
+                position={[5, 10, 5]}
+                intensity={0.8}
+                castShadow
+            />
+            <spotLight // Ajoute une lumière ponctuelle
+                position={[0, 5, 5]} 
+                angle={0.8} // Angle plus large
+                penumbra={1} // 
+                intensity={100} // Intensité élevée pour un éclairage fort
+                castShadow
                 shadow-mapSize={1024}
             />
-            <ambientLight intensity={0.7} />
+            {/* <ambientLight intensity={0.4} /> */}
 
-            <pointLight intensity={1} />
+            <pointLight 
+                intensity={6} 
+                position={[1, -0.2, 0]}
+            />
+
             {/* Ajoute l'ordinateur à la scène */}
             <primitive
-                object={computer.scene}
-                scale={isMobile ? 0.7 : 0.75}
-                position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-                rotation={[-0.01, -0.2, -0.1]}
+                object={computer.scene} // Utilise le modèle 3D de l'ordinateur
+                scale={isMobile ? 0.6 : 0.8} // Ajuste la taille de l'ordinateur
+                position={isMobile ? [0, -1, -2.2] : [0, -2.5, -1.5]} // Positionne l'ordinateur
+                rotation={[0, 0, 0]} // Ajuste la rotation de l'ordinateur
             />
         </mesh>
     );
@@ -54,20 +67,25 @@ const ComputersCanvas = () => {
     return (
         // Crée un canvas pour afficher l'ordinateur
         <Canvas
-            frameloop="demand"
-            shadows
-            dpr={[1, 2]}
-            camera={{ position: [20, 3, 5], fov: 25 }}
-            gl={{ preserveDrawingBuffer: true }}
+            frameloop="demand" // Optimise les performances
+            shadows // Active les ombres
+            dpr={[1, 2]} // Ajuste le taux de rafraîchissement
+            camera={{ position: [20, 3, 5], fov: 25 }} // Positionne la caméra
+            gl={{ preserveDrawingBuffer: true }} // Préserve le tampon de dessin
         >
             {/* Ajoute les contrôles de l'orbite */}
             <Suspense fallback={<CanvasLoader />}>
                 <OrbitControls
                     enableZoom={false}
-                    maxPolarAngle={Math.PI / 2}
-                    minPolarAngle={Math.PI / 2}
+                    maxPolarAngle={Math.PI / 2} 
+                    minPolarAngle={Math.PI / 2.4}
+                    // pour la rotation verticale et horizontale
+                    // maxPolarAngle={Math.PI}
+                    // minPolarAngle={0} 
+                    
                 />
                 <Computers isMobile={isMobile} />
+
             </Suspense>
 
             <Preload all />
