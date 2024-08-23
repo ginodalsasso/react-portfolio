af0d720a3916d2e0dc05a547cd317c42d6da0c4a
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-08-23 21:00:40 +0200

    balls sections

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+    Decal,
+    Float,
+    OrbitControls,
+    Preload,
+    useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = () => {
-  return (
-    <div>Ball</div>
-  )
-}
 
-export default Ball
+const Ball = (props) => {
+    const [decal] = useTexture([props.imgUrl]);
+
+    return (
+        // Crée une sphère flottante
+        <Float speed={1.2} rotationIntensity={1} floatIntensity={2}>  
+            <ambientLight intensity={0.3} /> 
+            <directionalLight position={[0, 0, 0.05]} />
+            <mesh castShadow receiveShadow scale={2.75}> 
+                <icosahedronGeometry args={[1, 1]} /> 
+                <meshStandardMaterial // Ajoute un matériau standard à la sphère
+                    color={'#fff8eb'}
+                    polygonOffset
+                    polygonOffsetFactor={-5}
+                    flatShading
+                />
+                <Decal 
+                    position={[0, 0, 1]}
+                    rotation={[2 * Math.PI, 0, 6.25]}
+                    flatShading
+                    map={decal}
+                />
+            </mesh>
+        </Float>
+    )
+};
+
+const BallCanvas = ( {icon} ) => {
+    return (
+        <Canvas
+            frameloop="demand" // Optimise les performances
+            gl={{ preserveDrawingBuffer: true }} // Préserve le tampon de dessin
+        >
+            {/* Ajoute les contrôles de l'orbite */}
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls enableZoom={false} />
+                <Ball imgUrl={icon} />
+
+            </Suspense>
+
+            <Preload all />
+        </Canvas>
+    )
+};
+
+export default BallCanvas;
